2126c655360652c7da31ba7dd7374b59bbbc1565
Markus Wind <markuswind@gmail.com>
2019-09-05 10:55:17 +0200

    feat: added base of jokes list component

--- src/app/modules/jokes/components/jokes-list/jokes-list.component.ts
+++ src/app/modules/jokes/components/jokes-list/jokes-list.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Joke } from '../../jokes.types';
+
 @Component({
   selector: 'app-jokes-list',
   templateUrl: './jokes-list.component.html',
   styleUrls: ['./jokes-list.component.scss']
 })
 export class JokesListComponent implements OnInit {
+  jokes: Joke[] = [];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.getJokes();
   }
 
+  getJokes() {
+    this.jokes = [
+      {
+        id: 0,
+        joke: 'Everybody loves Raymond. Except Chuck Norris.',
+        categories: ['tv', 'raymond']
+      },
+      {
+        id: 1,
+        joke: 'Chuck Norris his keyboard has the Any key.',
+        categories: []
+      }
+    ];
+  }
 }
--- src/app/modules/jokes/jokes.module.ts
+++ src/app/modules/jokes/jokes.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { JokesPageComponent } from './pages/jokes/jokes.page';
 import { JokesListComponent } from './components/jokes-list/jokes-list.component';
 
 @NgModule({
   declarations: [JokesPageComponent, JokesListComponent],
-  imports: [],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [JokesPageComponent]
 })
