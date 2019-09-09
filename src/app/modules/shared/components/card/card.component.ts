da224ba3c44ae8e54ec430ac316a58fd9b46fcb7
Markus Wind <markuswind@gmail.com>
2019-09-09 10:34:33 +0200

    feat: added base of card component

---
+++ src/app/modules/shared/components/card/card.component.ts
@@ -0,0 +1,11 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.scss'],
+  encapsulation: ViewEncapsulation.ShadowDom
+})
+export class CardComponent {
+  constructor() {}
+}
