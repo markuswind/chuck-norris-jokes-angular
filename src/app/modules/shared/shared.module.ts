385e3550ab4bac054b8e8f14e9cfbacb7c7eb5d1
Markus Wind <markuswind@gmail.com>
2019-09-10 09:43:42 +0200

    feat: added base of button component

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 
+import { ButtonComponent } from './components/button/button.component';
 import { CardComponent } from './components/card/card.component';
 
 @NgModule({
-  declarations: [CardComponent],
+  declarations: [ButtonComponent, CardComponent],
   imports: [],
-  exports: [CardComponent]
+  exports: [ButtonComponent, CardComponent]
 })
 export class SharedModule {}
